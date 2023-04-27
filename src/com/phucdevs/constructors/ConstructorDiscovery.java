412fcb7edaf997525ffb75aaa66cbdef368f4df1
phuc.letan <phuc.letan@messagemedia.com>
2023-04-27 15:58:26 +0700

    Constructor discovery example

---
+++ src/com/phucdevs/constructors/ConstructorDiscovery.java
@@ -0,0 +1,110 @@
+package com.phucdevs.constructors;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ConstructorDiscovery {
+
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        printConstructorDate(Person.class);
+        printConstructorDate(Address.class);
+
+        Address address = createInstanceWithArguments(Address.class, "First Street", 10);
+        Person person = createInstanceWithArguments(Person.class, address, "John", 20);
+        System.out.println(person);
+    }
+
+    static <T> T createInstanceWithArguments(Class<T> clazz, Object... args)
+            throws InvocationTargetException, InstantiationException, IllegalAccessException {
+
+        for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
+            if (constructor.getParameterTypes().length == args.length) {
+                return (T) constructor.newInstance(args);
+            }
+        }
+
+        System.out.println("An appropriate constructor was not found");
+        return null;
+    }
+    
+    static void printConstructorDate(Class<?> clazz) {
+
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        System.out.println(String.format("class %s has %d declared constructors",
+                clazz.getSimpleName(),
+                constructors.length));
+        
+        for (int i = 0; i < constructors.length; i++) {
+            Class<?>[] parameterTypes = constructors[i].getParameterTypes();
+
+            List<String> parameterTypeNames = Arrays.stream(parameterTypes)
+                    .map(type -> type.getSimpleName())
+                    .collect(Collectors.toList());
+
+            System.out.println(parameterTypeNames);
+        }
+    }
+
+    static class Person {
+
+        private final Address address;
+        private final String name;
+        private final int age;
+
+        public Person() {
+            this.name = "anonymous";
+            this.age = 0;
+            this.address = null;
+        }
+
+        public Person(String name) {
+            this.name = name;
+            this.age = 0;
+            this.address = null;
+        }
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+            this.address = null;
+        }
+
+        public Person(Address address, String name, int age) {
+            this.address = address;
+            this.name = name;
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "address=" + address +
+                    ", name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+
+    static class Address {
+
+        private final String street;
+        private int number;
+
+        public Address(String street, int number) {
+            this.street = street;
+            this.number = number;
+        }
+
+        @Override
+        public String toString() {
+            return "Address{" +
+                    "street='" + street + '\'' +
+                    ", number=" + number +
+                    '}';
+        }
+    }
+}
